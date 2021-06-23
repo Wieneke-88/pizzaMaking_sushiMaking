b45b60c022a955c513a0c5f121411585e5530bf5
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-23 20:26:58 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,32 @@
+const pizzaMaking = function () {
+    console.log('Knead dough');
+    console.log('Use toppings');
+    console.log('Bake it');
+};
+
+const sushiMaking = function () {
+    console.log('Make rice');
+    console.log('Add ingredients');
+    console.log('Eat with chopsticks');
+};
+
+const daftPunk = function () {
+    console.log('Buy it');
+    console.log('Use it');
+    console.log('Break it');
+    console.log('Fix it');
+    console.log('Trash it');
+    console.log('Change it');
+    console.log('Mail');
+    console.log('Upgrade it');
+    console.log('Charge it');
+    console.log('Point it');
+    console.log('Zoom it');
+}
+
+sushiMaking();
+sushiMaking();
+sushiMaking();
+
+pizzaMaking();
+pizzaMaking();
