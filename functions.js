d9437abfd262f17e9d0afac226096269f38e0b29
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-07-01 12:37:20 +0200

    Add files via upload
    1. Defining a function 
    2. Calling of functions 
    3. Begrepen, staat op Github

---
+++ functions.js
@@ -0,0 +1,20 @@
+//INHOUD: OPZET FUNCTION-DEFENITION 
+//      HEB IK BEGREPEN - staat in deze versie op GitHub 
+
+const pizzaMaking = function () {
+    console.log('Knead dough');
+    console.log('Use toppings');
+    console.log('Bake it');
+    console.log('Two orders for Pizza');
+}
+
+const sushiMaking = function () {
+    console.log('Make rice');
+    console.log('Add ingredients');
+    console.log('Eat with chopsticks');
+    console.log('Three orders for Sushi');
+}
+
+sushiMaking();
+pizzaMaking();
+
